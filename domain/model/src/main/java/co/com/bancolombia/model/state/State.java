b1698d2fc9e5f9acfcb250e550222a4061104aab
Esteban Sanchez <exloz@hotmail.com>
2025-08-27 14:02:30 -0500

    create database schema and docker-compose db config

--- domain/model/src/main/java/co/com/bancolombia/model/state/State.java
+++ domain/model/src/main/java/co/com/bancolombia/model/state/State.java
@@ -2,13 +2,14 @@ package co.com.bancolombia.model.state;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-//import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-//@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class State {
+    private Long id;
+    private String name;
+    private String description;
 }
