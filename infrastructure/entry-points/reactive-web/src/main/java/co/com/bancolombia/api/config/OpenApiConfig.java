64ce0eb3da820522346e28bfd6438fc85f1511b6
Esteban Sanchez <exloz@hotmail.com>
2025-09-03 14:05:58 -0500

    add RestConsumer with circuit breaker and configuration

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/config/OpenApiConfig.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/config/OpenApiConfig.java
@@ -2,8 +2,6 @@ package co.com.bancolombia.api.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.License;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
