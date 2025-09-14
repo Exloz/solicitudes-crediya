6b00ee37c2a27ca9eb7da43938a688adc016231b
Esteban Sanchez <exloz@hotmail.com>
2025-09-14 01:12:49 -0500

    refactor: remove generic QueueGateway interface
    - Delete the old generic QueueGateway<T> interface
    - Complete the migration to specific gateway interfaces
    - Clean up unused code and dependencies

--- domain/model/src/main/java/co/com/bancolombia/model/loanapplication/gateways/QueueGateway.java
+++
@@ -1,9 +0,0 @@
-package co.com.bancolombia.model.loanapplication.gateways;
-
-import co.com.bancolombia.model.loanapplication.LoanApplication;
-import reactor.core.publisher.Mono;
-
-public interface NotificationGateway {
-
-    Mono<String> sendStatusChangeNotification(LoanApplication application, String clientEmail);
-}
